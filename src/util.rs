ba51cd00138c44ddadf1d1f87bcb9272b6541751
Philipp Polterauer <philipp.polterauer@gmail.com>
2023-12-07 21:37:38 +0100

    refactored

--- src/util.rs
+++ src/util.rs
@@ -1,10 +1,5 @@
 pub fn load_file(day: i32, part: i32, runtest: bool) -> String {
-    let teststr;
-    if runtest {
-        teststr = "test_"
-    } else {
-        teststr = ""
-    }
+    let teststr = if runtest { "test_" } else { "" };
 
     let path = std::format!("data/day{day}/{teststr}input{part}.txt");
     println!("loading data from '{}'", path);
