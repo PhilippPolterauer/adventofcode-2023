973c2d2161a4d79af5880cf26e9c430e28adc72a
Philipp Polterauer <philipp.polterauer@gmail.com>
2023-12-10 22:00:03 +0100

    made everything work with criterion

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,15 +1,39 @@
-use adventofcode::days::day1;
+use adventofcode::days::*;
 use adventofcode::util::load_file;
 use criterion::{criterion_group, criterion_main, Criterion};
 
-pub fn criterion_benchmark(c: &mut Criterion) {
-    let input = load_file(2, 1, false, "data");
+#[macro_export]
+macro_rules! make_benchmark {
+    ($c:expr, $dayn:expr, $part:expr) => {
+        let input = load_file($dayn, $part, false, "data");
+        let func = dispatch_function($dayn, $part);
+        $c.bench_function(&format!("day{}::part{}", $dayn, $part), |b| {
+            b.iter(|| func(&input))
+        });
+    };
+}
 
-    c.bench_function("day1::part1", |b| {
-        b.iter(|| {
-            day1::part1(&input);
-        })
-    });
+pub fn criterion_benchmark(c: &mut Criterion) {
+    make_benchmark!(c, 1, 1);
+    make_benchmark!(c, 1, 2);
+    make_benchmark!(c, 2, 1);
+    make_benchmark!(c, 2, 2);
+    make_benchmark!(c, 3, 1);
+    make_benchmark!(c, 3, 2);
+    make_benchmark!(c, 4, 1);
+    make_benchmark!(c, 4, 2);
+    make_benchmark!(c, 5, 1);
+    make_benchmark!(c, 5, 2);
+    make_benchmark!(c, 6, 1);
+    make_benchmark!(c, 6, 2);
+    make_benchmark!(c, 7, 1);
+    make_benchmark!(c, 7, 2);
+    make_benchmark!(c, 8, 1);
+    make_benchmark!(c, 8, 2);
+    make_benchmark!(c, 9, 1);
+    make_benchmark!(c, 9, 2);
+    make_benchmark!(c, 10, 1);
+    make_benchmark!(c, 10, 2);
 }
 
 criterion_group!(benches, criterion_benchmark);
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::time::Instant;
 
 use clap::{arg, Parser};
 
+use crate::days::dispatch_function;
+
 /// Solving adventofcode challenges
 #[derive(Parser)]
 struct Cli {
@@ -40,61 +42,6 @@ struct Cli {
     numruns: i64,
 }
 
-fn get_function(day: i32, part: i32) -> fn(&str) -> i64 {
-    use days::*;
-    match (day, part) {
-        (1, 1) => day1::part1,
-        (1, 2) => day1::part2,
-        (2, 1) => day2::part1,
-        (2, 2) => day2::part2,
-        (3, 1) => day3::part1,
-        (3, 2) => day3::part2,
-        (4, 1) => day4::part1,
-        (4, 2) => day4::part2,
-        (5, 1) => day5::part1,
-        (5, 2) => day5::part2,
-        (6, 1) => day6::part1,
-        (6, 2) => day6::part2,
-        (7, 1) => day7::part1,
-        (7, 2) => day7::part2,
-        (8, 1) => day8::part1,
-        (8, 2) => day8::part2,
-        (9, 1) => day9::part1,
-        (9, 2) => day9::part2,
-        (10, 1) => day10::part1,
-        (10, 2) => day10::part2,
-        (11, 1) => day11::part1,
-        (11, 2) => day11::part2,
-        (12, 1) => day12::part1,
-        (12, 2) => day12::part2,
-        (13, 1) => day13::part1,
-        (13, 2) => day13::part2,
-        (14, 1) => day14::part1,
-        (14, 2) => day14::part2,
-        (15, 1) => day15::part1,
-        (15, 2) => day15::part2,
-        (16, 1) => day16::part1,
-        (16, 2) => day16::part2,
-        (17, 1) => day17::part1,
-        (17, 2) => day17::part2,
-        (18, 1) => day18::part1,
-        (18, 2) => day18::part2,
-        (19, 1) => day19::part1,
-        (19, 2) => day19::part2,
-        (20, 1) => day20::part1,
-        (20, 2) => day20::part2,
-        (21, 1) => day21::part1,
-        (21, 2) => day21::part2,
-        (22, 1) => day22::part1,
-        (22, 2) => day22::part2,
-        (23, 1) => day23::part1,
-        (23, 2) => day23::part2,
-        (24, 1) => day24::part1,
-        (24, 2) => day24::part2,
-        _ => |_| 0,
-    }
-}
-
 fn main() {
     let args = Cli::parse();
 
@@ -109,7 +56,7 @@ fn main() {
     } = args;
     let input = util::load_file(day, part, runtest, &data);
 
-    let function = get_function(day, part);
+    let function = dispatch_function(day, part);
 
     if profile {
         for _ in 0..numruns {
