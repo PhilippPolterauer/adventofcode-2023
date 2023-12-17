bba9ec5a43d128548d92faa27ba474e69216ea19
Philipp Polterauer <philipp.polterauer@gmail.com>
2023-12-17 15:59:23 +0100

    day15 part1

--- src/days/day15.rs
+++ src/days/day15.rs
@@ -1,5 +1,22 @@
-pub fn part1(_input: &str) -> i64 {
-    0
+fn hash(chars: &str) -> i64 {
+    let mut hash = 0;
+    for c in chars.chars() {
+        if c == '\n' {
+            continue;
+        }
+        hash += c as i64;
+        hash *= 17;
+        hash = hash % 256;
+    }
+    hash
+}
+
+pub fn part1(input: &str) -> i64 {
+    let mut solution = 0;
+    for split in input.split(',').into_iter() {
+        solution += hash(split);
+    }
+    solution
 }
 pub fn part2(_input: &str) -> i64 {
     0
